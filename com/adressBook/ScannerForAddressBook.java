821223ac8fa2bd674631f5a2548ef2d25563e87d
manjunathhalli <manjunathhalli629@gmail.com>
2021-08-15 20:45:04 +0530

    Ability to Read or Write

---
+++ com/adressBook/ScannerForAddressBook.java
@@ -0,0 +1,10 @@
+package com.adressBook;
+
+import java.util.Scanner;
+
+public class ScannerForAddressBook {
+    public Scanner scannerProvider(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+}
